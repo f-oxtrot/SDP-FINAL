b2b3e90e41683cfb71cdbc0b2fc0fc89c80de96f
MD FARHAN SHAHRIAR <farhanshahriarnabil@gmail.com>
2025-01-10 04:40:16 +0600

    tasin done

--- LB/Frontend/src/Components/DoctorDashboard.jsx
+++ LB/Frontend/src/Components/DoctorDashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import backgroundVideo from '../assets/Images/hos.mp4'; // Replace with actual video path
+import logo from '../assets/Images/logo.png'; // Replace with actual logo path
 
 const DoctorDashboard = () => {
   return (
     <div className="doctor-dashboard">
+      <img src={logo} alt="LifeBeacon Logo" className="logo" />
       <main className="main-content">
         <video autoPlay loop muted className="background-video">
           <source src={backgroundVideo} type="video/mp4" />
